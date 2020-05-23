75f80aecf1d581baa178ffdd955476dc526f3dfb
Faruk D <f.diblen@esciencecenter.nl>
2020-05-23 15:15:18 +0200

    add logging for zenodo call

--- src/zenodo.js
+++ src/zenodo.js
@@ -13,11 +13,11 @@ var moment = require('moment-timezone');
  * when done, send new facets to client.
  *
  * @params {Session} SPOT session
- * @return {Promise} A promise for emitting 'syncFacets' on the socket
+ * @return {Promise} A promise for emitting 'XXX' on the socket
  */
 
 function uploadZenodo (session) {
-
+    console.log('Zenodo Upload called.')
 }
 
 
